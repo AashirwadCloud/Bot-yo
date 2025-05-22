bd3c2646cdefed1a74a191371556a08e97c6bb01
AashirwadCloud <159162740+AashirwadCloud@users.noreply.github.com>
2025-05-22 09:22:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import random
 import json
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 bot.remove_command("help")
 
 @bot.event
